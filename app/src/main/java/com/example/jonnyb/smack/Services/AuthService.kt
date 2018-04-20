f41ec847c15015875f3933983e3504a6416eb504
Cacious Siamunyanga <csiamunyanga@hotmail.com>
2018-04-20 18:08:32 +0800

    initial commit of ultraSight

---
+++ app/src/main/java/com/example/jonnyb/smack/Services/AuthService.kt
@@ -0,0 +1,18 @@
+package com.example.jonnyb.smack.Services
+
+import android.content.Context
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.jonnyb.smack.Utilities.URL_REGISTER
+import org.json.JSONObject
+
+/**
+ * Created by jonnyb on 9/1/17.
+ */
+object AuthService {
+
+   
+}
---
+++ app/src/main/java/com/example/jonnyb/smack/Utilities/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.jonnyb.smack.Utilities
+
+/**
+ * Created by jonnyb on 9/1/17.
+ */
+
+const val BASE_URL = "https://devslopes-chattin.herokuapp.com/v1/"
+const val URL_REGISTER = "${BASE_URL}account/register"
